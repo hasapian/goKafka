4e8ed327ff591152d3aed49719ca03c5becbc671
hasapian <spiderman8787@gmail.com>
2022-07-14 13:53:18 +0300

    Topic created in TC

--- kafkaBrokers_test.go
+++ kafkaBrokers_test.go
@@ -3,10 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
+	"net"
 	"strconv"
 	"sync"
 	"testing"
 
+	"github.com/segmentio/kafka-go"
 	"github.com/stretchr/testify/require"
 )
 
@@ -47,9 +49,39 @@ func TestMultipleBrokers(t *testing.T) {
 
 	brokerAddresses := []string{broker1Address, broker2Address, broker3Address}
 
-	var chans [3]chan map[string]string
-	for x := range chans {
-		chans[x] = make(chan map[string]string)
+	conn, err := kafka.Dial("tcp", broker1Address)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer conn.Close()
+
+	controller, err := conn.Controller()
+	if err != nil {
+		panic(err.Error())
+	}
+	var controllerConn *kafka.Conn
+	controllerConn, err = kafka.Dial("tcp", net.JoinHostPort(controller.Host, strconv.Itoa(controller.Port)))
+	if err != nil {
+		panic(err.Error())
+	}
+	defer controllerConn.Close()
+
+	topicConfigs := []kafka.TopicConfig{
+		{
+			Topic:             topic,
+			NumPartitions:     3,
+			ReplicationFactor: 3,
+		},
+	}
+
+	err = controllerConn.CreateTopics(topicConfigs...)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var channels [3]chan map[string]string
+	for x := range channels {
+		channels[x] = make(chan map[string]string)
 	}
 
 	// produce messages in a new go routine, since
@@ -60,20 +92,20 @@ func TestMultipleBrokers(t *testing.T) {
 	for consumerId := 0; consumerId < 3; consumerId++ {
 		fmt.Println("Main: Starting consumer", consumerId)
 		wg.Add(1)
-		go consumeMultiple(&wg, ctx, brokerAddresses, topic, consumerId, t, chans[consumerId])
+		go consumeMultiple(&wg, ctx, brokerAddresses, topic, consumerId, t, channels[consumerId])
 	}
 
 	fmt.Println("Main: Waiting for consumers to finish")
 
 	var consumerMessages [3]map[string]string
-	consumerMessages[0] = <-chans[0]
-	consumerMessages[1] = <-chans[1]
-	consumerMessages[2] = <-chans[2]
+	consumerMessages[0] = <-channels[0]
+	consumerMessages[1] = <-channels[1]
+	consumerMessages[2] = <-channels[2]
 
 	allMessages := make(map[string]string)
 
 	for k := 0; k < 3; k++ {
-		close(chans[k])
+		close(channels[k])
 		for finalKey, finalValue := range consumerMessages[k] {
 			allMessages[finalKey] = finalValue
 		}
